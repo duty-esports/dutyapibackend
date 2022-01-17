e98f69ed7a3c268ab1796c5d8b0d6499eddbc090
Valdecir Ribeiro <valdecirribeiro>
2022-01-17 15:05:52 -0300

    alterado v3

--- cleardbJSw.session.sql
+++ cleardbJSw.session.sql
@@ -1,1 +1,9 @@
-create * FROM dutybd.user;
+
+CREATE TABLE USER (
+     name  varchar(255),
+     dataDeNascimento Date,
+     email  varchar(255),
+     password  varchar(255),
+      nickName  varchar(255),
+      plataformaDeJogo  varchar(255),
+);
