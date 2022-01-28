79a5afaabccc03e95f9063efb69db58f914dbe26
Valdecir Ribeiro <valdecirribeiro>
2022-01-27 22:01:37 -0300

    "jwmysql"

--- src/dataBase/database.providers.ts
+++ src/dataBase/database.providers.ts
@@ -5,11 +5,17 @@ export const databaseProviders = [
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
       type: 'mysql',
+      host: 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      port: 3306,
+      username: 'jplwf8cp6g5m5yba',
+      password: 'mzbv0x1z9aat5b95',
+      database: 'mmehlrfywjrwx3jb',
+    /*  type: 'mysql',
       host: 'us-cdbr-east-05.cleardb.net',
       port: 3306,
       username: 'b4e865e9c872c8',
       password: 'ec20e7d5',
-      database: 'dutybd',
+      database: 'dutybd',*/
      /* type: 'mysql',
       host: 'localhost',
       port: 3306,
