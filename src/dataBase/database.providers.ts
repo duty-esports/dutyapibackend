c065a3b29109e63c0be14450579f8a379289beeb
Valdecir Ribeiro <valdecirribeiro>
2022-02-02 21:14:25 -0300

    adicionado a licença do software, alterado o Readme, modificaçoes na busca

--- src/dataBase/database.providers.ts
+++ src/dataBase/database.providers.ts
@@ -4,18 +4,18 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
-      type: 'mysql',
+      /*type: 'mysql',
       host: 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       port: 3306,
       username: 'g03ncere83umpw8j',
       password: 'vpilawp71trpxaey',
-      database: 'm0q8hgxl9fg65uqi',
-     /* type: 'mysql',
+      database: 'm0q8hgxl9fg65uqi',*/
+      type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'sev1025',
-      database: 'dutybd',*/
+      database: 'dutybd',
       entities: [
           __dirname +'/../**/*.entity{.ts,.js}',
       ],
