9010ec616a93acbc7ece5ee90892151651cafd9d
Valdecir Ribeiro <valdecirribeiro>
2022-02-07 21:20:46 -0300

    alteraçoes

--- src/dataBase/database.providers.ts
+++ src/dataBase/database.providers.ts
@@ -4,13 +4,13 @@ export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async () => await createConnection({
-      type: 'mysql',
+       type: 'mysql',
       host: 'kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
       port: 3306,
       username: 'g03ncere83umpw8j',
       password: 'vpilawp71trpxaey',
       database: 'm0q8hgxl9fg65uqi',
-    /*  type: 'mysql',
+   /*  type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
